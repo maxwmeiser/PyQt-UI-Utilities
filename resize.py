fdf961c55d908c2115644553d37a506cf77fabee
Maxwell Meiser <maxwmeiser@icloud.com>
2022-09-20 21:25:48 -0600

    script calculates scale factors from command line input

--- resize.py
+++ resize.py
@@ -11,13 +11,56 @@ class worker():
     def __init__(self, argv):
         
         #declare command line arguments 
-        self.inPath = argv[1]
-        self.inRes = argv[2]
-        self.outRes = argv[3]
-        self.outPath = argv[4]
+        try:
+            self.inPath = argv[1]
+            self.inRes = argv[2]
+            self.outRes = argv[3]
+            self.outPath = argv[4]
+        except:
+            print("[ERROR] Missing command line argument! See readme for format.")
+            quit()
 
         #declare gen use variables
         self.wFactor = 1
         self.hFactor = 1
         self.workStr = ""
+
+    #this function isolates the width and height of the resolution input argument
+    def getResolution(self, getSplit):
+        #find x to create substrings 
+        i = 0
+        for x in getSplit:
+            i += 1
+            if x == 'x':
+                break
+        
+        #no x, invalid resolution format 
+        if i == 0:
+            print("[ERROR] Invalid resolution format! See readme for format")
+            quit()
+        
+        #return a touple (width,length)
+        resoList = (int(getSplit[:i-1]),int(getSplit[i:]))
+        return resoList
+
+    #this function calculates the scale factors for resizing
+    def getFactors(self):
+        #get inRes, outRes
+        splitInRes = self.getResolution(self.inRes)
+        splitOutRes = self.getResolution(self.outRes)
+
+        #divide both width and height to find scale factor
+        self.wFactor = splitOutRes[0] / splitInRes[0]
+        self.hFactor = splitOutRes[1] / splitInRes[1]
+
+        print(self.wFactor)
+        print(self.hFactor)
+        
+        
+
+        
+
+obj = worker(sys.argv)
+obj.getFactors()
+            
         
